3e6115002020ce1525b1b14536248346ec4feaef
paskovm <metodi_paskov@yahoo.com>
2025-02-25 17:21:50 +0200

    Added categories

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://api.chucknorris.io/jokes/random/";
+const API_URL = "https://api.chucknorris.io/jokes";
 
 app.get("/", async (req, res) => {
     try {
-        const result = await axios.post(API_URL + "/")
-        res.render("index.ejs");
-
+        const result = await axios.get(API_URL + "/categories");
+        res.render("index.ejs", {
+            categories: result.data
+        });
 
     } catch (error) {
         console.log(error.response.data);
-        res.statusCode(404);
+        res.status(500);
     }
 
 });
