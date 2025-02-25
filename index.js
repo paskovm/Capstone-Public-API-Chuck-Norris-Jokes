2054d891549e966ed1b29464790b6f2f539a0823
paskovm <metodi_paskov@yahoo.com>
2025-02-25 20:35:18 +0200

    added categories, cleaned issue with submit, added content to the .ejs

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 const API_URL = "https://api.chucknorris.io/jokes";
+var categories;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
     try {
         const result = await axios.get(API_URL + "/categories");
+        categories = result.data;
         res.render("index.ejs", {
-            categories: result.data
+            categories: categories
         });
-
     } catch (error) {
         console.log(error.response.data);
         res.status(500);
+    }
+});
+
+app.post("/", async (req, res) => {
+    try {
+        const category = req.body.category;
+        const result = await axios.get(API_URL + "/random" + (category ? `?category=${category}` : ""));
+        res.render("index.ejs", {
+            categories: categories,
+            content: result.data.value
+        });
+
+    } catch (error) {
+        res.render("index.ejs", {
+            content : error.message
+        });
     }
 
 });
